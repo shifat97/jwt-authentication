894646b5afcc15a27ab18166fd4e5e6d5c18e7d1
shifat97 <shifatbinreza2@gmail.com>
2025-09-19 00:13:24 +0600

    add: Database connection configs

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,2 +1,21 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const MONGO_CONN = process.env.MONGO_CONN;
+const DB_NAME = process.env.DB_NAME;
+
+const connectDB = async () => {
+  try {
+    const mongoURI = MONGO_CONN;
+    mongoose.connect(mongoURI, {
+      dbName: DB_NAME,
+    });
+    console.log("Database connection successful");
+  } catch (error) {
+    console.log("Database connection failed");
+  }
+};
+
+export default connectDB;
